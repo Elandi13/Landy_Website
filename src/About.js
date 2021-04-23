0c6a9c952e0a35ecb687eca031a9691acda66985
Elvis Landi <elandi13@yahoo.com>
2021-04-23 13:36:02 -0400

    Added info to the about component

--- src/About.js
+++ src/About.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            About
+            <h1> Who are we? </h1>
+            <p> 
+                We are the lead distrubters in BBCOS hair products, based in New York City. We offer various products to keep your hair in prestine condition. We are here to help find the right product for you. Contact us today for more information! 
+            </p>
+
+            
+
         </div>
     )
 }
